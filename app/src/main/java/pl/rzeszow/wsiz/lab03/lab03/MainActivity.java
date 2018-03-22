a3d0465c15f42c574ccb68da535dace550762a21
Michal-l\Majkel <mih.pago@op.pl>
2018-03-22 18:57:50 +0100

    Pierwsze zmiany

--- app/src/main/java/pl/rzeszow/wsiz/lab03/lab03/MainActivity.java
+++ app/src/main/java/pl/rzeszow/wsiz/lab03/lab03/MainActivity.java
@@ -24,9 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         CheckBox checkBox = (CheckBox) findViewById(R.id.checkbox);
 
-        RadioButton rbOpcja1 = (RadioButton) findViewById(R.id..rbOpcja1);​
-        RadioButton rbOpcja2 = (RadioButton) findViewById(R.id.r​bOpcja2); ​
-        RadioButton rbOpcja3 = (RadioButton) findViewById(R.id.​rbOpcja3);
+        RadioButton rbOpcja1 = (RadioButton) findViewById(R.id.rbOpcja1);
+        RadioButton rbOpcja2 = (RadioButton) findViewById(R.id.rbOpcja2);
+        RadioButton rbOpcja3 = (RadioButton) findViewById(R.id.rbOpcja3);
 
         String[] planety = {"Merkury","Wenus","Ziemia","Mars","Jowisz","Saturn","Uran","Neptun"};
         spinner = findViewById(R.id.spinner);
@@ -34,15 +34,29 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, planety);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); spinner.setAdapter(adapter);
 
-        View.OnClickListener listener = ​new ​View.OnClickListener() {
-            @Override​
-            public void ​ onClick(View v) {​
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 RadioButton radioButton = (RadioButton) v;
-                if​ (radioButton.isChecked()) {​
-                        Log.​i(​"TAG"​, ​"Zaznaczona opcja: " ​+ radioButton.getText()); ​
+                String wartosc = "";
+                if(radioButton.isChecked())
+                {
+                    return;
                 }
+
+                if(v.getId() == R.id.rbOpcja1)
+                {
+                    wartosc = "Zaznaczono opcję 1";
+                }
+                else if(v.getId() == R.id.rbOpcja2)
+                {
+                    wartosc = "Zaznaczono opcję 2";
+                }
+
+                Toast.makeText(MainActivity.this, wartosc, Toast.LENGTH_SHORT).show();
             }
         };
+
 
         rbOpcja1.setOnClickListener(listener);
         rbOpcja2.setOnClickListener(listener);
